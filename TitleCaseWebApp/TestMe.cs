fb1cd508d0ca197ba4fb78bffed507ce7b0e0cb5
Sneha <67682184+SnehaBaskarann@users.noreply.github.com>
2024-02-21 22:40:11 +0530

    Add files via upload

---
+++ TitleCaseWebApp/TestMe.cs
@@ -0,0 +1,26 @@
+﻿namespace TitleCaseWebApp
+{
+    public class TestMe
+    {
+        
+            public string MakeTitle(string input)
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                    return input;
+
+                string[] words = input.Split(' ');
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (!string.IsNullOrWhiteSpace(words[i]))
+                    {
+                        char[] wordChars = words[i].ToCharArray();
+                        wordChars[0] = char.ToUpper(wordChars[0]);
+                        words[i] = new string(wordChars);
+                    }
+                }
+
+                return string.Join(' ', words);
+            }
+        }
+    }
+
